0177f668afb9a6f33c3aad276c3271548d57af28
DashaVan <43934226+DashaVan@users.noreply.github.com>
2021-08-23 14:09:16 +0300

    lesson 1.1 done

--- HelloWorld/app/src/main/java/spacekotlin/vaniukova/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/spacekotlin/vaniukova/helloworld/MainActivity.kt
@@ -1,11 +1,14 @@
 package spacekotlin.vaniukova.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val LOG_TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(LOG_TAG, "onCreate")
     }
 }
