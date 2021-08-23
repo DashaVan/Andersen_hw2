4f7fffa3da893345264be2dfae2d03f65cd04c89
DashaVan <43934226+DashaVan@users.noreply.github.com>
2021-08-23 15:38:01 +0300

    lesson 1.2/A done

--- HelloToast/app/src/main/java/spacekotlin/vaniukova/hellotoast/MainActivity.kt
+++ HelloToast/app/src/main/java/spacekotlin/vaniukova/hellotoast/MainActivity.kt
@@ -1,11 +1,35 @@
 package spacekotlin.vaniukova.hellotoast
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import spacekotlin.vaniukova.hellotoast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var mCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonToast.setOnClickListener {
+            showToast()
+        }
+
+        binding.buttonCount.setOnClickListener {
+            countUp()
+        }
+    }
+
+    private fun showToast() {
+        Toast.makeText(applicationContext, R.string.toast_message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun countUp() {
+        mCount++
+        binding.showCount.text = mCount.toString()
     }
 }
